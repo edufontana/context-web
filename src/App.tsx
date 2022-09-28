df93ba46bbbe1da8aa7ab4f7ad4e545521776925
edu <edufontana7@gmail.com>
2022-09-27 21:05:14 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ShowData } from './pages/ShowData'
+import { SignUp } from './pages/SignUp'
 
+export function App() {
   return (
-    <div>
-     1
-    </div>
+    <BrowserRouter>
+      <h1>Titulo</h1>
+      <hr />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/exibir" element={<ShowData />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
---
+++ src/pages/ShowData.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+export function ShowData() {
+  return (
+    <div>
+      <Link to="/">showdata</Link>
+    </div>
+  )
+}
